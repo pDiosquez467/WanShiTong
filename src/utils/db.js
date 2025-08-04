086e76f2fe7d6b315cbde0db70bb507bebfb6ffa
pDiosquez467 <pabloDiosquez467@gmail.com>
2025-08-04 16:36:03 -0300

    Agrego refac 'executeQuery'

--- src/utils/db.js
+++ src/utils/db.js
@@ -1,10 +1,12 @@
 
-const executeQuery = async (dbClient, query) => {
-    try {
-        return await dbClient.query(query)
-    } catch (error) {
-        throw { status: error?.status ?? 500, message: error?.message ?? error }
-    }
+async function executeQuery(query) {
+  try {
+    return await dbClient.query(query);
+  } catch (err) {
+    const status = err.status || 500;
+    const message = err.message || 'Internal Server Error';
+    throw { status, message };
+  }
 }
 
 module.exports = {
