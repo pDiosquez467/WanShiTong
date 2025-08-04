7ffa6c5f98f51e196fbd9f7cd405f60d8f14943c
pDiosquez467 <pabloDiosquez467@gmail.com>
2025-08-04 16:47:28 -0300

    Agrego refac para centralizar manejo de errores y facilitar legibilidad

--- src/models/usersModel.js
+++ src/models/usersModel.js
@@ -1,114 +1,86 @@
 const dbClient = require('../db/conn')
 
+const { executeQuery } = require('../utils/db')
+
+const TABLE = 'users'
+
 const getAllUsers = async () => {
-    try {
-        const query = {
-            name: 'get-all-users',
-            text: `SELECT * FROM users;`
-        }
-        return await dbClient.query(query)
-    } catch (error) {
-        throw { status: error?.status ?? 500, message: error?.message ?? error }
+    const query = {
+        name: 'get-all',
+        text: `SELECT * FROM ${TABLE};`
     }
+    const res = await executeQuery(dbClient, query)
+    return res.rows
 }
 
 const getOneUser = async (userId) => {
-    try {
-        const query = {
-            name: 'get-user',
-            text: `SELECT * FROM users WHERE id = $1;`,
-            values: [userId]
-        }
-
-        const res = await dbClient.query(query)
-
-        if (res.rows.length === 0) {
-            throw { status: 404, message: `Can't find user with ID '${userId}'` }
-        }
-
-        return res.rows[0]
-    } catch (error) {
-        throw { status: error?.status ?? 500, message: error?.message ?? error }
+    const query = {
+        name: 'get-user',
+        text: `SELECT * FROM ${TABLE} WHERE id = $1;`,
+        values: [userId]
     }
+
+    const res = await executeQuery(dbClient, query)
+
+    if (res.rows.length === 0) {
+        throw { status: 404, message: `Can't find user with ID '${userId}'` }
+    }
+    return res.rows[0]
 }
 
 const createOneUser = async (newUser) => {
-    try {
-        const { name, email, password, entry_date } = newUser
+    entry_date = new Date().toISOString()
+    const { name, email, password } = newUser
 
-        const query = {
-            name: 'create-user',
-            text: `INSERT INTO users (name, email, password, entry_date) 
+    const query = {
+        name: 'create-user',
+        text: `INSERT INTO ${TABLE} (name, email, password, entry_date) 
         VALUES ($1, $2, $3, $4) RETURNING *;`,
-            values: [name, email, password, entry_date]
-        }
-
-        const created = await dbClient.query(query)
-        return created.rows[0]
-    } catch (error) {
-        throw { status: error?.status ?? 500, message: error?.message ?? error }
+        values: [name, email, password, entry_date]
     }
 
+    const created = await executeQuery(dbClient, query)
+    return created.rows[0]
 }
 
 const updateOneUser = async (userId, changes) => {
-    const fields = [] 
-    const values = []
+    const fields = [], values = []
+    Object.entries(changes).forEach(([key, val], i) => {
+        fields.push(`${key} = $${i + 1}`)
+        values.push(val);
+    })
 
-    if (changes.name !== undefined) {
-        fields.push(`name = $${values.length + 1}`)
-        values.push(changes.name)
+    if (fields.length === 0) throw { status: 400, message: 'Nothing to update!' }
+
+    values.push(id)
+    const query = {
+        text: `UPDATE ${TABLE} SET ${fields.join(', ')} WHERE id = $${values.length} RETURNING *;`,
+        values,
     }
 
-    if (changes.email !== undefined) {
-        fields.push(`email = $${values.length + 1}`)
-        values.push(changes.email)
+    const res = await executeQuery(dbClient, query)
+
+    if (res.rows.length === 0) {
+        throw { status: 404, message: `Can't find user with ID '${userId}'` }
     }
 
-    if (changes.password !== undefined) {
-        fields.push(`password = $${values.length + 1}`)
-        values.push(changes.password)
-    }
-
-    values.push(userId)
-    const idIndex = values.length
-
-    try {
-        const query = {
-            text: `UPDATE users SET ${fields.join(', ')} WHERE id = $${idIndex} RETURNING *;`,
-            values
-        }
-
-        const updated = await dbClient.query(query)
-
-        if (updated.rows.length === 0) {
-            throw { status: 404, message: `Can't find user with ID '${userId}'` }
-        }
-
-        return updated.rows[0]
-    } catch (error) {
-        throw { status: error?.status ?? 500, message: error?.message ?? error }
-    }
+    return res.rows[0]
 }
 
 const deleteOneUser = async (userId) => {
-    try {
-        const query = {
-            name: 'delete-user',
-            text: `DELETE FROM users WHERE id = $1 RETURNING *;`,
-            values: [userId]
-        }
-
-        const deleted = await dbClient.query(query)
-
-        if (deleted.rows.length === 0) {
-            throw { status: 404, message: `Can't find user with ID '${userId}'` }
-        }
-
-        return deleted.rows[0]
-    } catch (error) {
-        throw { status: error?.status ?? 500, message: error?.message ?? error }
+    const query = {
+        name: 'delete-user',
+        text: `DELETE FROM ${TABLE} WHERE id = $1 RETURNING *;`,
+        values: [userId]
     }
+
+    const deleted = await executeQuery(dbClient, query)
+
+    if (deleted.rows.length === 0) {
+        throw { status: 404, message: `Can't find user with ID '${userId}'` }
+    }
+
+    return deleted.rows[0]
 }
 
 module.exports = {
